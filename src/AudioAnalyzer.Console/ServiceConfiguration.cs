e22008bb0d380a77d5c0552b07a8e2093caa897d
CzarSibelius <12322677+CzarSibelius@users.noreply.github.com>
2026-02-21 20:47:36 +0200

    simplified registrations

--- src/AudioAnalyzer.Console/ServiceConfiguration.cs
+++ src/AudioAnalyzer.Console/ServiceConfiguration.cs
@@ -62,58 +62,24 @@ internal static class ServiceConfiguration
             sp.GetRequiredService<IConsoleWriter>(),
             sp.GetRequiredService<UiSettings>()));
 
-        services.AddSingleton<IVisualizationRenderer>(sp =>
-        {
-            var dimensions = sp.GetRequiredService<IDisplayDimensions>();
-            var visualizers = sp.GetServices<IVisualizer>();
-            var visualizerSettings = sp.GetRequiredService<VisualizerSettings>();
-            var uiSettings = sp.GetRequiredService<UiSettings>();
-            var viewportFactory = sp.GetRequiredService<IScrollingTextViewportFactory>();
-            return new VisualizationPaneLayout(dimensions, visualizers, visualizerSettings, uiSettings, viewportFactory);
-        });
+        services.AddSingleton<IVisualizationRenderer, VisualizationPaneLayout>();
         services.AddSingleton<IBeatDetector, BeatDetector>();
         services.AddSingleton<IVolumeAnalyzer, VolumeAnalyzer>();
         services.AddSingleton<IFftBandProcessor, FftBandProcessor>();
-        services.AddSingleton<AnalysisEngine>(sp =>
-        {
-            var renderer = sp.GetRequiredService<IVisualizationRenderer>();
-            var dimensions = sp.GetRequiredService<IDisplayDimensions>();
-            var beatDetector = sp.GetRequiredService<IBeatDetector>();
-            var volumeAnalyzer = sp.GetRequiredService<IVolumeAnalyzer>();
-            var fftBandProcessor = sp.GetRequiredService<IFftBandProcessor>();
-            return new AnalysisEngine(renderer, dimensions, beatDetector, volumeAnalyzer, fftBandProcessor);
-        });
+        services.AddSingleton<AnalysisEngine>();
         services.AddSingleton<ShowPlaybackController>();
         services.AddSingleton<IScrollingTextEngine, ScrollingTextEngine>();
         services.AddSingleton<IScrollingTextViewportFactory, ScrollingTextViewportFactory>();
-        services.AddSingleton<ITitleBarRenderer>(sp => new TitleBarRenderer(
-            sp.GetRequiredService<UiSettings>(),
-            sp.GetRequiredService<VisualizerSettings>(),
-            sp.GetServices<IVisualizer>()));
+        services.AddSingleton<ITitleBarRenderer, TitleBarRenderer>();
         services.AddSingleton<IDeviceSelectionModal, DeviceSelectionModal>();
         services.AddSingleton<IHelpModal, HelpModal>();
-        services.AddSingleton<ISettingsModalRenderer>(sp =>
-        {
-            var factory = sp.GetRequiredService<IScrollingTextViewportFactory>();
-            return new SettingsModalRenderer(
-                sp.GetRequiredService<VisualizerSettings>(),
-                sp.GetRequiredService<UiSettings>(),
-                sp.GetRequiredService<IPaletteRepository>(),
-                factory);
-        });
+        services.AddSingleton<ISettingsModalRenderer, SettingsModalRenderer>();
         services.AddSingleton<ISettingsModalKeyHandler, SettingsModalKeyHandler>();
         services.AddSingleton<ISettingsModal, SettingsModal>();
         services.AddSingleton<IShowEditModal, ShowEditModal>();
         services.AddSingleton<IMainLoopKeyHandler, MainLoopKeyHandler>();
-        services.AddSingleton<IDeviceCaptureController>(sp => new DeviceCaptureController(
-            sp.GetRequiredService<IAudioDeviceInfo>(),
-            sp.GetRequiredService<AnalysisEngine>()));
-        services.AddSingleton<IAppSettingsPersistence>(sp => new AppSettingsPersistence(
-            sp.GetRequiredService<AnalysisEngine>(),
-            sp.GetRequiredService<VisualizerSettings>(),
-            sp.GetRequiredService<AppSettings>(),
-            sp.GetRequiredService<ISettingsRepository>(),
-            sp.GetRequiredService<IVisualizerSettingsRepository>()));
+        services.AddSingleton<IDeviceCaptureController, DeviceCaptureController>();
+        services.AddSingleton<IAppSettingsPersistence, AppSettingsPersistence>();
         services.AddSingleton<IHeaderDrawer>(sp =>
         {
             var factory = sp.GetRequiredService<IScrollingTextViewportFactory>();
@@ -125,26 +91,7 @@ internal static class ServiceConfiguration
                 sp.GetRequiredService<AnalysisEngine>(),
                 sp.GetRequiredService<UiSettings>());
         });
-        services.AddSingleton<ApplicationShell>(sp =>
-        {
-            return new ApplicationShell(
-                sp.GetRequiredService<IDeviceCaptureController>(),
-                sp.GetRequiredService<IVisualizerSettingsRepository>(),
-                sp.GetRequiredService<VisualizerSettings>(),
-                sp.GetRequiredService<IPresetRepository>(),
-                sp.GetRequiredService<IShowRepository>(),
-                sp.GetRequiredService<IPaletteRepository>(),
-                sp.GetRequiredService<AnalysisEngine>(),
-                sp.GetRequiredService<IVisualizationRenderer>(),
-                sp.GetRequiredService<ShowPlaybackController>(),
-                sp.GetRequiredService<IHeaderDrawer>(),
-                sp.GetRequiredService<IMainLoopKeyHandler>(),
-                sp.GetRequiredService<IAppSettingsPersistence>(),
-                sp.GetRequiredService<IDeviceSelectionModal>(),
-                sp.GetRequiredService<IHelpModal>(),
-                sp.GetRequiredService<ISettingsModal>(),
-                sp.GetRequiredService<IShowEditModal>());
-        });
+        services.AddSingleton<ApplicationShell>();
 
         return services.BuildServiceProvider();
     }
